62c59289481d4f49a197a1b908de2555856f00a3
Mutombo <27485850+DeMutombo@users.noreply.github.com>
2023-01-12 22:49:55 +0200

    Updated navitions form home page to different launching page.

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -3,14 +3,15 @@ import React from "react";
 
 export const Navbar = () => {
   return (
-    <nav>
+    <nav className="bg-gray-100 p-6 fixed font-medium w-full mx-auto">
       <div className="container flex justify-between">
         <div>
           <Link href={"/"}>Logo</Link>
         </div>
-        <div>
+        <div className="flex gap-5">
           <Link href={"/about"}>About</Link>
-          <Link href={"/History"}>History</Link>
+          <Link href={"/launches"}>Launches</Link>
+          <Link href={"/contact-us"}>Contact us</Link>
         </div>
         <div>Login</div>
       </div>
